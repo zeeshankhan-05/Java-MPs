ac854c744ea4f5aa1fddace78328bae518ad238f
zeeshankhan-05 <khanzeeshan1205@gmail.com>
2024-05-26 10:18:39 -0500

    Create MP 1 - Face Fun

---
+++ FaceFun.java
@@ -0,0 +1,14 @@
+public class FaceFun {
+
+    public static void main(String[] args) {
+        
+        System.out.println("  \n  //////"); // Hair
+        System.out.println(" |     |");  // Forehead
+        System.out.println(" | \" \" |");  // Eyebrows
+        System.out.println(" | 0 0 |"); // Eyes
+        System.out.println("(|  ^  |)"); // Ears
+        System.out.println(" | \\_/ |"); // Mouth
+        System.out.println(" |_____|"); // Chin
+
+    }
+}
